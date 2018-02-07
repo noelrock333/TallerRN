6d21acdb19195cd744a820207764438d5fca000f
Noel Escobedo <noel@Noels-MacBook-Pro.local>
2018-02-07 22:05:07 +0000

    Usando React Navigation de la manera mas basica

--- components/Login/index.js
+++ components/Login/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, Button, TextInput, Alert } from 'react-native';
 import Home from '../Home';
+import { StackNavigator } from 'react-navigation';
 
 class Login extends React.Component {
   state = {
@@ -11,31 +12,42 @@ class Login extends React.Component {
 
   login = () => {
     const { username, password } = this.state;
-    this.setState({ showLogin: false });
+    this.props.navigation.navigate('Home');
   };
 
   render() {
-    if (this.state.showLogin) {
-      return (
-        <View style={{ paddingTop: 30, backgroundColor: '#E9EAED', flex: 1 }}>
-          <Text style={{ marginBottom: 10 }}>Login screen</Text>
-          <TextInput
-            style={{ backgroundColor: 'white', padding: 10, marginBottom: 10 }}
-            placeholder="Username"
-            onChangeText={text => { this.setState({ username: text })}}
-          />
-          <TextInput
-            style={{ backgroundColor: 'white', padding: 10 }}
-            placeholder="Password"
-            onChangeText={text => { this.setState({ password: text })}}
-          />
-          <Button title="Login" onPress={this.login} />
-        </View>
-      );
-    } else {
-      return (<Home />);
-    }
+    return (
+      <View style={{ paddingTop: 30, backgroundColor: '#E9EAED', flex: 1 }}>
+        <Text style={{ marginBottom: 10 }}>Login screen</Text>
+        <TextInput
+          style={{ backgroundColor: 'white', padding: 10, marginBottom: 10 }}
+          placeholder="Username"
+          onChangeText={text => { this.setState({ username: text })}}
+        />
+        <TextInput
+          style={{ backgroundColor: 'white', padding: 10 }}
+          placeholder="Password"
+          onChangeText={text => { this.setState({ password: text })}}
+        />
+        <Button title="Login" onPress={this.login} />
+      </View>
+    );
   }
 }
 
-export default Login;
+const RootStack = StackNavigator(
+  {
+    Login: {
+      screen: Login,
+    },
+    Home: {
+      screen: Home,
+    },
+  },
+  {
+    initialRouteName: 'Login',
+  }
+);
+
+
+export default RootStack;
