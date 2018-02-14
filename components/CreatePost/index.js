0cb6feef9d5df5dcaef6bf0f979fc21cab9fb9ff
Noel Escobedo <noel@Noels-MacBook-Pro.local>
2018-02-14 03:53:41 +0000

    Linkeada libreria CameraRoll en ios

--- components/CreatePost/index.js
+++ components/CreatePost/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import { View, TouchableOpacity, ScrollView, Button, Image, CameraRoll } from 'react-native';
 import { Content, Header, Container, Body, Text, Icon, Right, Left } from 'native-base';
 import Api from '../../utils/api';
 
 class CreatePost extends React.Component {
+  state = {
+    photos: []
+  };
+
   uploadFile = () => {
     Api.postImage().then(data => {
       console.log(data);
     });
+  };
+
+  _handleButtonPress = () => {
+    CameraRoll.getPhotos({
+        first: 20,
+        assetType: 'Photos',
+      })
+      .then(r => {
+        this.setState({ photos: r.edges });
+      })
+      .catch((err) => {
+         //Error Loading Images
+      });
   };
 
   render() {
@@ -25,9 +42,24 @@ class CreatePost extends React.Component {
           <Right />
         </Header>
         <Content>
-          <TouchableOpacity onPress={this.uploadFile}>
+          {/* <TouchableOpacity onPress={this.uploadFile}>
             <Icon name="ios-share" />
-          </TouchableOpacity>
+          </TouchableOpacity> */}
+          <Button title="Load Images" onPress={this._handleButtonPress} />
+          <ScrollView>
+            {this.state.photos.map((p, i) => {
+            return (
+              <Image
+                key={i}
+                style={{
+                  width: 300,
+                  height: 100,
+                }}
+                source={{ uri: p.node.image.uri }}
+              />
+            );
+          })}
+          </ScrollView>
         </Content>
       </Container>
     );
