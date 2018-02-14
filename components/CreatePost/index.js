2002e744eccf0f07bff35f3f4783bce8d66bbfa2
Noel Escobedo <noel@Noels-MacBook-Pro.local>
2018-02-14 05:41:34 +0000

    Agregamos y linkeamos el paquete para tomar fotos y acceder a la galeria

--- components/CreatePost/index.js
+++ components/CreatePost/index.js
@@ -1,12 +1,51 @@
 import React from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import { View, TouchableOpacity, Image, StyleSheet } from 'react-native';
 import { Content, Header, Container, Body, Text, Icon, Right, Left } from 'native-base';
 import Api from '../../utils/api';
+import ImagePicker from 'react-native-image-picker';
+
+var options = {
+  title: 'Select Avatar',
+  customButtons: [
+    {name: 'fb', title: 'Choose Photo from Facebook'},
+  ],
+  storageOptions: {
+    skipBackup: true,
+    path: 'images'
+  }
+};
 
 class CreatePost extends React.Component {
+  state = {
+    avatarSource: null
+  };
+
   uploadFile = () => {
-    Api.postImage().then(data => {
-      console.log(data);
+    // Api.postImage().then(data => {
+    //   console.log(data);
+    // });
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+    
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      }
+      else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      }
+      else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      }
+      else {
+        let source = { uri: response.uri };
+    
+        // You can also display the image using data:
+        // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+    
+        this.setState({
+          avatarSource: source
+        });
+      }
     });
   };
 
@@ -26,12 +65,19 @@ class CreatePost extends React.Component {
         </Header>
         <Content>
           <TouchableOpacity onPress={this.uploadFile}>
-            <Icon name="ios-share" />
+            <Text>Obtener Imagen</Text>
           </TouchableOpacity>
+          <Image source={this.state.avatarSource} style={styles.uploadAvatar} />
         </Content>
       </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  uploadAvatar: {
+    height: 200
+  }
+});
 
 export default CreatePost;
