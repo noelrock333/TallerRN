c7ffef088b8b56260e56a6f4b301243930777987
Noel Escobedo <noel@Noels-MacBook-Pro.local>
2018-02-21 18:03:30 +0000

    Ya se puede agregar un comentario usando la api

--- components/Comments/index.js
+++ components/Comments/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import { View, TouchableOpacity, StyleSheet } from 'react-native';
 import {
   Container,
   Header,
@@ -12,13 +12,37 @@ import {
   Item,
   Input
 } from 'native-base';
+import Api from '../../utils/api';
 
 class Comments extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
     const { comments, id } = this.props.navigation.state.params;
-    const comentarios = comments.map(item => (
-      <View>
-        <Text>{item}</Text>
+    this.state = {
+      comments: comments,
+      id: id
+    };
+  }
+
+  postComment = () => {
+    const comment = {
+      content: this.state.comment
+    };
+
+    Api.post(`/posts/${this.state.id}/comments`, { comment }).then(data => {
+      if (data.status == 200) {
+        this.setState({
+          comments: [...this.state.comments, comment],
+          comment: ''
+        });
+      }
+    });
+  };
+
+  render() {
+    const comentarios = this.state.comments.map((item, index) => (
+      <View key={index} style={styles.comment}>
+        <Text>{item.content}</Text>
       </View>
     ));
 
@@ -40,9 +64,10 @@ class Comments extends React.Component {
           <Item rounded>
             <Input
               placeholder="Escribe un comentario"
-              onChangeText={title => this.setState({ title })}
+              onChangeText={comment => this.setState({ comment })}
+              value={this.state.comment}
             />
-            <TouchableOpacity onPress={() => console.log('algo')}>
+            <TouchableOpacity onPress={this.postComment}>
               <Icon active name="ios-send" />
             </TouchableOpacity>
           </Item>
@@ -51,5 +76,13 @@ class Comments extends React.Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  comment: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ebebeb'
+  }
+});
 
 export default Comments;
