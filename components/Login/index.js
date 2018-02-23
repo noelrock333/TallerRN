4b867ca77c1c11d46a5d975e25c83cdf9442ad78
Noel Escobedo <noel@Noels-MacBook-Pro.local>
2018-02-23 00:04:21 +0000

    Agregamos un fondo y estilo a la pagina de login

--- components/Login/index.js
+++ components/Login/index.js
@@ -1,55 +1,125 @@
 import React from 'react';
-import { View, Text, Button, TextInput, Alert } from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  TextInput,
+  Alert,
+  StyleSheet,
+  ImageBackground,
+  TouchableOpacity
+} from 'react-native';
 import Spinner from 'react-native-loading-spinner-overlay';
 
 class Login extends React.Component {
   state = {
     username: '',
-    password: '',
+    password: ''
   };
 
   login = () => {
-    const { username, password } = this.state;
+    // const { username, password } = this.state;
     const useraccount = {
-      "auth": {
-          "email": "noel@michelada.io",
-          "password": "12345678"
+      auth: {
+        email: 'noel@michelada.io',
+        password: '12345678'
       }
     };
 
     this.setState({ showSpinner: true });
-    this.props.screenProps.login(useraccount).then(() => {
-      this.setState({ showSpinner: false });
-    })
-    .catch(() => {
-      this.setState({ showSpinner: false }, () => {
-        setTimeout(() => {
-          Alert.alert('Invalid username or password');
-        }, 0);
+    this.props.screenProps
+      .login(useraccount)
+      .then(() => {
+        this.setState({ showSpinner: false });
+      })
+      .catch(() => {
+        this.setState({ showSpinner: false }, () => {
+          setTimeout(() => {
+            Alert.alert('Usuario o password invalido');
+          }, 0);
+        });
       });
-    });
   };
 
   render() {
     return (
-      <View style={{ paddingTop: 30, backgroundColor: '#E9EAED', flex: 1 }}>
-        <Text style={{ marginBottom: 10 }}>Login screen</Text>
-        <TextInput
-          style={{ backgroundColor: 'white', padding: 10, marginBottom: 10 }}
-          placeholder="Username"
-          onChangeText={text => { this.setState({ username: text })}}
-        />
-        <TextInput
-          style={{ backgroundColor: 'white', padding: 10 }}
-          placeholder="Password"
-          onChangeText={text => { this.setState({ password: text })}}
-        />
-        <Button title="Login" onPress={this.login} />
+      <View style={styles.body}>
+        <ImageBackground
+          style={styles.titleBackground}
+          source={require('../../assets/background.jpeg')}
+        >
+          <Text style={styles.title}>InstagramApp</Text>
+        </ImageBackground>
+        <View style={styles.loginForm}>
+          <TextInput
+            style={[styles.textInput, { marginBottom: 10 }]}
+            placeholder="Username"
+            onChangeText={text => this.setState({ username: text })}
+          />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Password"
+            secureTextEntry={true}
+            onChangeText={text => this.setState({ password: text })}
+          />
+          <View style={styles.loginButtonWrapper}>
+            <Button title="Iniciar sesión" onPress={this.login} />
+          </View>
+          <View style={styles.forgotLabel}>
+            <Text style={{ color: 'gray', marginRight: 5 }}>
+              Olvidaste tu contraseña?
+            </Text>
+            <TouchableOpacity>
+              <Text style={{ color: '#2F9BEF' }}>Obten ayuda aqui</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
 
-        <Spinner visible={this.state.showSpinner} textContent={"Cargando..."} textStyle={{color: '#FFF'}} />
+        <Spinner
+          visible={this.state.showSpinner}
+          textContent="Cargando..."
+          textStyle={{ color: '#FFF' }}
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: 'white',
+    flex: 1
+  },
+  titleBackground: {
+    width: '100%',
+    height: 180,
+    justifyContent: 'center'
+  },
+  title: {
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 30
+  },
+  loginForm: {
+    padding: 10,
+    paddingTop: 20
+  },
+  textInput: {
+    backgroundColor: '#FAFAFA',
+    padding: 14,
+    borderColor: '#ebebeb',
+    borderWidth: 1,
+    borderRadius: 7
+  },
+  loginButtonWrapper: {
+    marginTop: 10,
+    marginBottom: 15
+  },
+  forgotLabel: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'center'
+  }
+});
 
 export default Login;
